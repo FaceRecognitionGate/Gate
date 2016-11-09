23e28900c32e8e661acdf7093d576e622ebb5870
Matheus Marotzke <matheusdmd@al.insper.edu.br>
2016-11-09 15:41:05 -0200

    /photo working - thanks Rachel

--- FRG/src/Package/TestServlet.java
+++ FRG/src/Package/TestServlet.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import Package.RaspiStill;
 
@@ -20,24 +21,29 @@ public class TestServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+		OutputStream responseOutputStream = response.getOutputStream();
 
-        PrintWriter writer = response.getWriter();
+        //PrintWriter writer = new PrintWriter(new OutputStreamWriter(responseOutputStream,"utf-8"));
 
         camera.TakePicture("image.png",800,600);
-        writer.write("data: "+ camera +"\r\n");
-        writer.flush();
-        writer.close();
+        //writer.write("data: "+ camera +"\r\n");
+        //writer.flush();
+        //writer.close();
 
 		String FileName = "image.png";
 		String contextPath = getServletContext().getRealPath(File.separator);
 		File pngFile = new File("/opt/apache-tomcat-9.0.0.M11/bin/" + FileName);
-
+		
+		//Runtime.getRuntime().exec("chmod 777 " + FileName);
+		pngFile.setReadable(true, false);
+		pngFile.setExecutable(true, false);
+		pngFile.setWritable(true, false);
+		
 		response.setContentType("image/png");
 		response.addHeader("Content-Disposition", "attachment; filename=" + FileName);
 		response.setContentLength((int) pngFile.length());
 		
 		FileInputStream fileInputStream = new FileInputStream(pngFile);
-		OutputStream responseOutputStream = response.getOutputStream();
 		int bytes;
 		while ((bytes = fileInputStream.read()) != -1) {
 			responseOutputStream.write(bytes);
